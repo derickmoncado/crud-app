a318d8a5cdf807d3ad5300daf30d062ddf01b425
Derick Moncado <derickmoncado@gmail.com>
2020-09-22 22:01:23 -0700

    Setting up crud app

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+
+import { IUser } from '../../models/User'
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+export class UsersComponent implements OnInit {
+  // Properties
+  public users: IUser[];
+  public showExtended: boolean = true;
+  public loaded: boolean = false ;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.users = [
+        {
+          firstName: 'Derick',
+          lastName: 'Moncado',
+          age: 32,
+          address: {
+            street: '7784 Envoy Way',
+            city: 'Citrus Heights',
+            state: 'CA',
+          },
+        },
+        {
+          firstName: 'Korbin',
+          lastName: 'Dallas',
+          age: 52,
+          address: {
+            street: '123 Flouaston Paradise',
+            city: 'New York City',
+            state: 'NY',
+          },
+        },
+        {
+          firstName: 'Ash',
+          lastName: 'Ketchum',
+          age: 17,
+          address: {
+            street: '1234 Some Street',
+            city: 'Pallet Town',
+            state: 'CA',
+          },
+        },
+      ];
+
+      this.loaded = true;
+    }, 2000);
+
+    this.showExtended = false;
+  }
+
+}
---
+++ src/app/models/User.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+  firstName: string;
+  lastName: string;
+  age: number;
+  address: {
+    street: string;
+    city: string;
+    state: string;
+  };
+}
