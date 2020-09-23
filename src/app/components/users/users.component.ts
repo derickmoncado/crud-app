656a022dbb386d378d4edd38bfbe8360236d102b
Derick Moncado <derickmoncado@gmail.com>
2020-09-22 22:16:09 -0700

    More crud work.

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -16,44 +16,50 @@ export class UsersComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.users = [
-        {
-          firstName: 'Derick',
-          lastName: 'Moncado',
-          age: 32,
-          address: {
-            street: '7784 Envoy Way',
-            city: 'Citrus Heights',
-            state: 'CA',
-          },
+    this.users = [
+      {
+        firstName: 'Derick',
+        lastName: 'Moncado',
+        age: 32,
+        address: {
+          street: '7784 Envoy Way',
+          city: 'Citrus Heights',
+          state: 'CA',
         },
-        {
-          firstName: 'Korbin',
-          lastName: 'Dallas',
-          age: 52,
-          address: {
-            street: '123 Flouaston Paradise',
-            city: 'New York City',
-            state: 'NY',
-          },
+      },
+      {
+        firstName: 'Korbin',
+        lastName: 'Dallas',
+        age: 52,
+        address: {
+          street: '123 Flouaston Paradise',
+          city: 'New York City',
+          state: 'NY',
         },
-        {
-          firstName: 'Ash',
-          lastName: 'Ketchum',
-          age: 17,
-          address: {
-            street: '1234 Some Street',
-            city: 'Pallet Town',
-            state: 'CA',
-          },
+      },
+      {
+        firstName: 'Ash',
+        lastName: 'Ketchum',
+        age: 17,
+        address: {
+          street: '1234 Some Street',
+          city: 'Pallet Town',
+          state: 'CA',
         },
-      ];
+      },
+    ];
 
-      this.loaded = true;
-    }, 2000);
+    this.loaded = true;
 
-    this.showExtended = false;
+    this.addUser({
+      firstName: 'David',
+      lastName: 'Jackson',
+    })
+  }
+
+  // Add User
+  addUser(user: IUser) {
+    this.users.push(user);
   }
 
 }
--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,10 +1,10 @@
 export interface IUser {
   firstName: string;
   lastName: string;
-  age: number;
-  address: {
-    street: string;
-    city: string;
-    state: string;
+  age?: number;
+  address?: {
+    street?: string;
+    city?: string;
+    state?: string;
   };
 }
